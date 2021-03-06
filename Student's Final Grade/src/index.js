c7ecf3178de253d499f8d057e5d9f5121e64aae5
ramkumar-1819 <rohitram1819@gmail.com>
2021-03-06 17:06:45 +0530

    Done

--- Student's Final Grade/src/index.js
+++ Student's Final Grade/src/index.js
@@ -13,7 +13,18 @@
 
 // Write the function to find the finalGrade obtained.
 function finalGrade (exam, projects) {
-         //your code here
+        if(exam>90 || projects>10){
+          return 100
+        }
+        else if(exam>75 && projects>=5){
+          return 90
+        }
+        else if(exam>50 && projects>=2){
+          return 75
+        }
+        else{
+          return 0
+        }
 
   }
 //Use SpecRunner to check the Test Cases.
